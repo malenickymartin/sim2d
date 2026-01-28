3f280bcca5b8ac267a0962d584b106c47dde0c2f
Martin Malenický <malenicky.martin@gmail.com>
2026-01-28 18:30:38 +0100

    residue loss

--- src/sim2d/gnn/losses.py
+++ src/sim2d/gnn/losses.py
@@ -60,26 +60,22 @@ class GNNLoss(torch.nn.Module):
         return F.l1_loss(pred_values, gt_values, weight=weight)
 
     def residue_loss(self, data, object_states, lambdas_dict) -> torch.Tensor:
-        dt = (
-            torch.ones(
-                (data["object"].x.shape[0], 1),
-                dtype=data["object"].x.dtype,
-                device=data["object"].x.device,
-            )
-            * self.dt
-        )
-        gravity = self.gravity.repeat(data["object"].x.shape[0], 1).to(data["object"].x.device)
+        device = data["object"].x.device
+        num_shapes = data["object"].x.shape[0]
+        dt = torch.ones((num_shapes, 1), dtype=data["object"].x.dtype, device=device) * self.dt
+        gravity = self.gravity.repeat(num_shapes, 1).to(device)
 
         restitutions = data["object"].x[:, 0].unsqueeze(1)
-        masses = data["object"].x[:, 1].unsqueeze(1)
-        v_init = data["object"].x[:, [2, 3, 5]]
+        masses = data["object"].x[:, 1]
+        inertias = data["object"].x[:, 2]
+        inv_masses = 1 / torch.stack([masses, masses, inertias], dim=1)
+        v_init = data["object"].x[:, [3, 4, 6]]
         v_pred = object_states
-
         res_v = v_pred - v_init - gravity * dt
-        total_impulse = torch.zeros_like(res_v)
-        res_c_list = []
 
-        for edge_type in OUTPUT_FEATURE_DIMS.keys():
+        total_vel_delta = torch.zeros_like(res_v)
+        res_c_list = []
+        for edge_type, num_constraints in OUTPUT_FEATURE_DIMS.items():
             if not isinstance(edge_type, tuple):
                 continue
             if edge_type not in data.edge_types or edge_type not in lambdas_dict:
@@ -89,48 +85,42 @@ class GNNLoss(torch.nn.Module):
             edge_attr = data[edge_type].edge_attr
             target_indices = edge_index[1]
             lambdas_all = lambdas_dict[edge_type]
-            is_contact = "contact" in edge_type
-            num_constraints = OUTPUT_FEATURE_DIMS[edge_type]
-            if is_contact:
-                jacobians = edge_attr[:, :3]
-                dists = edge_attr[:, 3].unsqueeze(1)
+            is_contact = "contact" in edge_type[1]
+            is_equality = not is_contact
 
-                impulse = -lambdas_all * jacobians
-                total_impulse.index_add_(0, target_indices, impulse)
+            target_indices = edge_index[1]
+            target_inv_masses = inv_masses[target_indices]
+            target_dt = dt[target_indices]
+            target_v_pred = v_pred[target_indices]
 
-                body_restitution = restitutions[target_indices]
-                body_v_init = v_init[target_indices]
-                body_v_pred = v_pred[target_indices]
-
-                b_restitution = body_restitution * body_v_init
-                v_term = body_v_pred + b_restitution
-                b_scaled = (jacobians * v_term).sum(dim=1, keepdim=True)
-
-                edge_dt = dt[target_indices]
-                b_error = -(self.beta / edge_dt) * dists
-
-                a = b_scaled + b_error
-                b = lambdas_all
-                fb = a + b - torch.sqrt(a**2 + b**2 + self.eps)
-                res_c_list.append(fb)
-            else:
-                edge_dt = dt[target_indices]
-                body_v_pred = v_pred[target_indices]
-                for k in range(num_constraints):
-                    jacobians = edge_attr[:, k * 4 : k * 4 + 3]
-                    dists = edge_attr[:, k * 4 + 3].unsqueeze(1)
+            for k in range(num_constraints):
+                jacobians = edge_attr[:, k * 4 : k * 4 + 3]
+                dists = edge_attr[:, k * 4 + 3].unsqueeze(1)
+                if num_constraints == 1:
+                    lambdas = lambdas_all
+                else:
                     lambdas = lambdas_all[:, k].unsqueeze(1)
-
-                    impulse = -lambdas * jacobians
-                    total_impulse.index_add_(0, target_indices, impulse)
-
-                    b_scaled = (jacobians * body_v_pred).sum(dim=1, keepdim=True)
-                    b_error = -(self.beta / edge_dt) * dists
-
-                    a = b_scaled + b_error
+                force_impulse = -lambdas * jacobians
+                vel_delta = force_impulse * target_inv_masses
+                total_vel_delta.index_add_(0, target_indices, vel_delta)
+                if is_equality:
+                    v_term = target_v_pred
+                else:
+                    target_v_init = v_init[target_indices]
+                    target_restitution = restitutions[target_indices]
+                    b_rest_val = target_restitution * target_v_init
+                    v_term = target_v_pred + b_rest_val
+                b_scaled = (jacobians * v_term).sum(dim=1, keepdim=True)
+                b_error = -(self.beta / target_dt) * dists
+                a = b_scaled + b_error
+                if is_equality:
                     res_c_list.append(a)
+                else:
+                    b = lambdas
+                    fb = a + b - torch.sqrt(a**2 + b**2 + self.eps)
+                    res_c_list.append(fb)
 
-        res_v = res_v + total_impulse / masses
+        res_v = res_v + total_vel_delta
         res_v_flat = res_v.flatten()
         if res_c_list:
             res_c_flat = torch.cat(res_c_list).flatten()
