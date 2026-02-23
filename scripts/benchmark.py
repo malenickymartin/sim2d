fixes: bias error for nodes without edges, restitution indexation, lambdas residue loss calculation

scripts/benchmark.py
@@ -116,13 +116,13 @@ def benchmark(dataset_root: str, model_name: str):
             SimGNN.solver.newton_iters = 0
 
             state_hybrid = SimHybrid.solver.step(0, state, contacts)
-            res_hybrid = torch.norm(Sim.solver.resudial_fn(state_hybrid, state, contacts)).item()
+            res_hybrid = torch.norm(Sim.solver.residual_fn(state_hybrid, state, contacts)).item()
 
             state_newton = SimNewton.solver.step(0, state, contacts)
-            res_newton = torch.norm(Sim.solver.resudial_fn(state_newton, state, contacts)).item()
+            res_newton = torch.norm(Sim.solver.residual_fn(state_newton, state, contacts)).item()
 
             state_gnn = SimGNN.solver.step(0, state, contacts)
-            res_gnn = torch.norm(Sim.solver.resudial_fn(state_gnn, state, contacts)).item()
+            res_gnn = torch.norm(Sim.solver.residual_fn(state_gnn, state, contacts)).item()
 
             res["hybrid"].append(res_hybrid)
             res["newton"].append(res_newton)

scripts/training.py
@@ -105,13 +105,14 @@ def train(
                 "learning_rate": scheduler.get_last_lr()[0],
             }
         )
-        wandb.save(str(config["dataset_root"]))
         if isinstance(scheduler, torch.optim.lr_scheduler.ReduceLROnPlateau):
             scheduler.step(val_loss["total_loss"])
         else:
             scheduler.step()
         if val_loss["total_loss"] < min_val_loss:
             min_val_loss = val_loss["total_loss"]
+            save_dir = config["dataset_root"] / "models"
+            save_dir.mkdir(parents=True, exist_ok=True)
             torch.save(model, config["dataset_root"] / "models" / config["model_name"])
 
 

src/sim2d/engine.py
@@ -50,7 +50,7 @@ class EulerSolver:
         ), f"State shape is not correct. Expected: {self.state_shape(constraints)}, got {state.shape}."
         with self.logger.timed_block("newton_solve"):
             for i in range(self.newton_iters):
-                res_val: torch.Tensor = self.resudial_fn(state, state_init, constraints)
+                res_val: torch.Tensor = self.residual_fn(state, state_init, constraints)
                 if torch.norm(res_val) < self.atol:
                     self.logger.log_engine_data(
                         step_idx, i, state.shape, res_val.detach(), torch.Tensor(), torch.Tensor()
@@ -63,7 +63,7 @@ class EulerSolver:
                         with torch.enable_grad():
                             state_var = state.detach().requires_grad_(True)
                             J = torch.autograd.functional.jacobian(
-                                lambda z: self.resudial_fn(z, state_init, constraints), state_var
+                                lambda z: self.residual_fn(z, state_init, constraints), state_var
                             )
                             if J.dim() > 2:
                                 J = J.view(J.shape[0], -1)
@@ -81,7 +81,7 @@ class EulerSolver:
                     return state.detach()
         return state.detach()
 
-    def resudial_fn(self, state: torch.Tensor, state_init: torch.Tensor, constraints):
+    def residual_fn(self, state: torch.Tensor, state_init: torch.Tensor, constraints):
         res = torch.zeros_like(state)
         res[:, 3:] = state[:, 3:]
         res[:, :3] = state[:, :3] - state_init[:, :3] - self.gravity * self.dt

src/sim2d/gnn/losses.py
@@ -94,16 +94,19 @@ class GNNLoss(torch.nn.Module):
 
             bodies = edges.edge_index[1, edge_idxs]
             neighs = edges.edge_index[0, edge_idxs] if is_obj_obj else torch.full_like(bodies, -1)
-            preds_all = lambdas_dict[(src, name, dst)][edge_idxs]
+            if is_obj_obj:
+                preds_all = (
+                    lambdas_dict[(src, name, dst)][edge_idxs]
+                    + lambdas_dict[(src, name, dst)][edge_idxs + 1]
+                ) / 2.0
+            else:
+                preds_all = preds_all
             for k in range(num_constraints):
                 attr_offset = k * 4
                 J_body = edges.edge_attr[edge_idxs, attr_offset : attr_offset + 3]
                 dists = edges.edge_attr[edge_idxs, attr_offset + 3]
                 if is_obj_obj:
-                    if is_joint:
-                        J_neigh = edges.edge_attr[edge_idxs + 1, attr_offset : attr_offset + 3]
-                    else:
-                        J_neigh = edges.edge_attr[edge_idxs + 1, attr_offset : attr_offset + 3]
+                    J_neigh = edges.edge_attr[edge_idxs + 1, attr_offset : attr_offset + 3]
                 else:
                     J_neigh = torch.zeros_like(J_body)
                 preds = preds_all[:, k]
@@ -152,7 +155,7 @@ class GNNLoss(torch.nn.Module):
                 v
             )
 
-        res_flat = self.dummy_solver.resudial_fn(state, state_init, constraints)
+        res_flat = self.dummy_solver.residual_fn(state, state_init, constraints)
         res_unflat_shape = self.dummy_solver.state_shape(constraints)
         res_unflat = res_flat.view(res_unflat_shape)
         vel_res = res_unflat[:, :3]

src/sim2d/gnn/network.py
@@ -132,10 +132,13 @@ class InteractionNetwork(MessagePassing):
     def message(self, edge_attr):
         return edge_attr
 
-    def update(self, aggr_out, x):
+    def update(self, aggr_out, x, has_incoming=None):
         if isinstance(x, tuple):
             x = x[1]
-        return self.mlp_node(torch.cat([x, aggr_out], dim=-1)) + x
+        x_updated = self.mlp_node(torch.cat([x, aggr_out], dim=-1)) + x
+        if has_incoming is not None:
+            x_updated = torch.where(has_incoming.unsqueeze(-1), x_updated, x)
+        return x_updated
 
     def forward(
         self,
@@ -147,8 +150,15 @@ class InteractionNetwork(MessagePassing):
             size = (x[0].size(0), x[1].size(0))
         else:
             size = (x.size(0), x.size(0))
+
+        has_incoming = torch.zeros(size[1], dtype=torch.bool, device=edge_index.device)
+        if edge_index.numel() > 0:
+            has_incoming[edge_index[1]] = True
+
         edge_attr_updated = self.edge_updater(edge_index, x=x, edge_attr=edge_attr)
-        x_updated = self.propagate(edge_index, x=x, edge_attr=edge_attr_updated, size=size)
+        x_updated = self.propagate(
+            edge_index, x=x, edge_attr=edge_attr_updated, size=size, has_incoming=has_incoming
+        )
         return x_updated, edge_attr_updated
 
 
@@ -185,8 +195,8 @@ class Processor(nn.Module):
                 x_updated, edge_attr_updated = layer["_".join(edge_type)](x, edge_index, edge_attr)
                 edge_attr_dict[edge_type] = edge_attr_updated
                 x_res_aggr[dst_type] += x_updated - x_dict[dst_type]
-            for node_type, x_res_aggr in x_res_aggr.items():
-                x_dict[node_type] = x_dict[node_type] + x_res_aggr
+            for node_type, res_aggr in x_res_aggr.items():
+                x_dict[node_type] = x_dict[node_type] + res_aggr
         return x_dict, edge_attr_dict
 
 

src/sim2d/simulator.py
@@ -259,7 +259,7 @@ class Simulator(ABC):
             (self.num_shapes, NODE_FEATURE_DIMS["object"]), dtype=torch.float32, device=self.device
         )
         for i in range(self.num_shapes):
-            gnn_data["object"].x[i][:] = torch.tensor(
+            gnn_data["object"].x[i][:] = torch.stack(
                 [
                     self.shapes[i].mass,
                     self.shapes[i].inertia,
@@ -267,9 +267,8 @@ class Simulator(ABC):
                     state[i][1],
                     torch.norm(state[i][:2]),
                     state[i][2],
-                ],
-                device=self.device,
-            )
+                ]
+            ).to(self.device)
 
         if (
             self.floor is not None
@@ -450,9 +449,10 @@ class Simulator(ABC):
             state_guess[:, :3] += dt * self.gravity
         else:
             gnn_data = self.create_gnn_data(state, constraints)
-            gnn_output = self.gnn(
-                gnn_data.x_dict, gnn_data.edge_index_dict, gnn_data.edge_attr_dict
-            )
+            with torch.no_grad():
+                gnn_output = self.gnn(
+                    gnn_data.x_dict, gnn_data.edge_index_dict, gnn_data.edge_attr_dict
+                )
             state_guess = self.state_from_gnn(gnn_output, constraints)
 
         return state_guess